546721740e8aa8153f2baf61e0cd6ef92840c7b6
Yesicanmendoza <yesica-mimi@live.com>
2022-10-08 04:24:13 +0000

    Two Sum

---
+++ sum_two.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        hmap = {}
+        
+        for i, n in enumerate(nums):
+            num = target - n
+            if num in hmap:
+                return [hmap[num], i]
+            else:
+                hmap[n] = i
